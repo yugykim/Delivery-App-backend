f23a9bb9711f36549096a70591cd35eb645e9837
yugykim <yugyeongk92@gmail.com>
2022-09-07 20:57:44 +0900

    using spyOn to test send,verifyEmail FN

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,17 +1,16 @@
 import { Test } from '@nestjs/testing';
 import { MailService } from './mail.service';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import FormData = require('form-data');
+import got from 'got';
+import { any } from 'joi';
 
 const API_KEY = 'APIKEY';
 const DOMAIN = 'DOMAIN';
 const FROM_EMAIL = 'FROMEMAIL';
 
-jest.mock('got', () => {
-  return {};
-});
-jest.mock('form-data', () => {
-  return { append: jest.fn() };
-});
+jest.mock('got');
+jest.mock('form-data');
 
 describe('MailService', () => {
   let service: MailService;
@@ -35,7 +34,42 @@ describe('MailService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+  it('sendVerificationEmail', () => {
+    const sendVerificationEmailArgs = { email: 'email', code: 'code' };
+    //spy on : it implement all of the function.
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    jest.spyOn(service, 'sendEmail').mockImplementation(async () => true);
+    service.sendVerificationEmail(
+      sendVerificationEmailArgs.email,
+      sendVerificationEmailArgs.code,
+    );
+    expect(service.sendEmail).toHaveBeenCalledTimes(1);
+    expect(service.sendEmail).toHaveBeenCalledWith(
+      'verify your email',
+      'confirm',
+      [
+        { key: 'code', value: sendVerificationEmailArgs.code },
+        { key: 'username', value: sendVerificationEmailArgs.email },
+      ],
+    );
+  });
 
-  it.todo('sendEmail');
-  it.todo('sendVerificationEmail');
+  it('sendEmail', async () => {
+    const ok = await service.sendEmail('test', 'test', []);
+    const formSpy = jest.spyOn(FormData.prototype, 'append');
+    expect(formSpy).toHaveBeenCalled();
+    expect(got.post).toHaveBeenCalledTimes(1);
+    expect(got.post).toBeCalledWith(
+      `https://api.mailgun.net/v3/${DOMAIN}/messages`,
+      expect.any(Object),
+    );
+    expect(ok).toEqual(true);
+  });
+  it('fails on error', async () => {
+    jest.spyOn(got, 'post').mockImplementation(() => {
+      throw new Error();
+    });
+    const ok = await service.sendEmail('test', 'test', []);
+    expect(ok).toEqual(false);
+  });
 });
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,13 +10,12 @@ export class MailService {
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {}
 
-  private async sendEmail(
+  async sendEmail(
     subject: string,
     template: string,
     eamilVars: EmailVar[],
-  ) {
+  ): Promise<boolean> {
     const form = new FormData();
-
     form.append('from', `Yugy from Ubereat <mailgun@${this.options.domain}>`);
     form.append('to', `yugyeongk92@gmail.com`);
     form.append('subject', subject);
@@ -24,17 +23,20 @@ export class MailService {
     eamilVars.forEach((eVar) => form.append(`v:${eVar.key}`, eVar.value));
 
     try {
-      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Basic ${Buffer.from(
-            `api:${this.options.apiKey}`,
-          ).toString('base64')}`,
+      await got.post(
+        `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `api:${this.options.apiKey}`,
+            ).toString('base64')}`,
+          },
+          body: form,
         },
-        body: form,
-      });
+      );
+      return true;
     } catch (error) {
-      console.log(error);
+      return false;
     }
   }
 
