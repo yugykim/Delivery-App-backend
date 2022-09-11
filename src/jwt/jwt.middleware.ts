0df1479448375078bbcb8e947ce4920195904015
yugykim <yugyeongk92@gmail.com>
2022-09-11 23:25:49 +0900

    completed finding user's profile e2e test

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -16,7 +16,7 @@ export class JwtMiddleware implements NestMiddleware {
       const decoded = this.jwtService.verify(token.toString());
       if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
         try {
-          const user = await this.UsersService.findById(decoded['id']);
+          const { user } = await this.UsersService.findById(decoded['id']);
           req['user'] = user;
           //req meet midleware first, so middle can modify the type of reqest.keep traveling down to resolver
           //if we send wrong token, it won't work
--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -226,7 +226,50 @@ describe('UserModule (e2e)', () => {
     });
   });
 
-  it.todo('me');
-  it.todo('verifyEmail');
+  describe('me', () => {
+    it('should return find my profile', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `{
+          me {
+            email,
+          }
+        }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                me: { email },
+              },
+            },
+          } = res;
+          expect(email).toBe(testUser.email);;
+        });
+    });
+    it('should not allow logged out user', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `{
+        me {
+          email,
+        }
+      }`,
+        })
+        .expect(200)
+        .expect((res) => {  
+          const {
+            body: { errors },
+          } = res;
+          const [error] = errors;
+          expect(error.message).toBe('Forbidden resource');
+        });
+    });
+  });
+  //// .set('X-JWT', jwtToken) << remove this not to log out user
   it.todo('editProfile');
 });
