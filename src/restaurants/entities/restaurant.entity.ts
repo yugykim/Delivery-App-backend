1a3cea1645cf8ad36254d21b69a06be01384111f
yugykim <yugyeongk92@gmail.com>
2022-09-17 22:57:33 -0600

    Restaurant schema and entity,name of input type

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,27 +1,38 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import { IsBoolean, IsOptional, Length } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsString, Length } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { Category } from './category.entity';
 
 //database model
+@InputType('RestaurantInputType', { isAbstract: true })
 @ObjectType() //graphql schema
 @Entity() //For typeOrm
-export class Restaurant {
-  @PrimaryGeneratedColumn()
-  @Field(() => Number)
-  id: number;
-
+export class Restaurant extends CoreEntity {
   @Field(() => String)
   @Column()
   @Length(5)
   name: string;
 
-  @Field(() => Boolean, { defaultValue: true })
-  @Column({ default: true })
-  @IsBoolean()
-  @IsOptional()
-  isVegan: boolean;
+  @Field(() => String)
+  @Column()
+  @IsString()
+  coverImage: string;
 
   @Field(() => String)
   @Column()
+  @IsString()
   address: string;
+
+  @Field(() => Category, { nullable: true }) // do not delete restaurant when we delete category
+  @ManyToOne(() => Category, (category) => category.restaurants, {
+    nullable: true,
+    onDelete: 'SET NULL', //no category is possible
+  })
+  category: Category;
+
+  @Field(() => User) // do not delete restaurant when we delete category
+  @ManyToOne(() => User, (user) => user.restaurants)
+  owner: User;
 }
