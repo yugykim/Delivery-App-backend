106802a8482834ef34995334f38f893bea469189
yugykim <yugyeongk92@gmail.com>
2022-09-18 23:55:38 -0600

    changed enum to able to keyof

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -12,10 +12,10 @@ import { IsBoolean, IsEmail, IsEnum, IsString } from 'class-validator';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 
 //=enumerate
-enum UserRole {
-  Owner,
-  Client,
-  Delivery,
+export enum UserRole {
+  Owner = 'Owner',
+  Client = 'Client',
+  Delivery = 'Delivery',
 }
 
 registerEnumType(UserRole, { name: 'UserRole' });
