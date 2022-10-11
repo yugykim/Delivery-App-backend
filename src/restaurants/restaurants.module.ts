ea4a23e98d81ec0ab11b5b927d33507f0b2a4189
yugykim <yugyeongk92@gmail.com>
2022-10-11 00:07:32 -0600

    add DishResolver in Providers

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
+import { Dish } from './entities/dish.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurant.service';
-import { CategoryResolver, RestaurantsResolver } from './restaurants.resolver';
+import {
+  CategoryResolver,
+  DishResolver,
+  RestaurantsResolver,
+} from './restaurants.resolver';
 
 //in restaurant module, we need restaurant repository
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant, Category])],
-  providers: [RestaurantsResolver, RestaurantService, CategoryResolver],
+  imports: [TypeOrmModule.forFeature([Restaurant, Category, Dish])],
+  providers: [
+    RestaurantsResolver,
+    RestaurantService,
+    CategoryResolver,
+    DishResolver,
+  ],
 })
 export class RestaurantsModule {}
