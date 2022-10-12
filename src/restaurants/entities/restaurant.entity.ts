20ccde8a924d57dfcd537033ac38738aeae4a984
yugykim <yugyeongk92@gmail.com>
2022-10-11 23:20:07 -0600

    add oneToMany order and rest Relationship

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,7 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { IsString, Length } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
+import { Order } from 'src/orders/entities/order.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Column, Entity, ManyToOne, OneToMany, RelationId } from 'typeorm';
 import { Category } from './category.entity';
@@ -28,6 +29,10 @@ export class Restaurant extends CoreEntity {
 
   @Field(() => Number)
   restaurantID: number;
+
+  @Field(() => [Order])
+  @OneToMany(() => Order, (order) => order.restaurant)
+  orders: Order[];
 
   @Field(() => Category, { nullable: true }) // do not delete restaurant when we delete category
   @ManyToOne(() => Category, (category) => category.restaurants, {
