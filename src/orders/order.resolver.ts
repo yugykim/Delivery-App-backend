105219ba6bde91e0f8c1386ad2f6d5d328aaee3c
yugykim <yugyeongk92@gmail.com>
2022-10-11 23:24:14 -0600

    build cerateOrder sending customer,CreateOrderT

---
+++ src/orders/order.resolver.ts
@@ -0,0 +1,21 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decoratior';
+import { User } from 'src/users/entities/user.entity';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { Order } from './entities/order.entity';
+import { OrderService } from './order.service';
+
+@Resolver(() => Order)
+export class OrderResolver {
+  constructor(private readonly ordersService: OrderService) {}
+
+  @Mutation(() => CreateOrderOutput)
+  @Role(['Customer'])
+  async createOrder(
+    @AuthUser() customer: User,
+    @Args('input') createOrderInput: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    return this.ordersService.createOrder(customer, createOrderInput);
+  }
+}
