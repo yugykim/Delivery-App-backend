a6582a8ea296a0dfeb9ed620bac6ed31f9e1b0b4
yugykim <yugyeongk92@gmail.com>
2022-10-11 23:22:36 -0600

    build order moudle having Order, ResT

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { Order } from './entities/order.entity';
+import { OrderResolver } from './order.resolver';
+import { OrderService } from './order.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Order, Restaurant])],
+  providers: [OrderResolver, OrderService],
+  exports: [OrderService],
+})
+export class OrdersModule {}
