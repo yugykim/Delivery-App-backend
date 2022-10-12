429038b8cebb688e93440372525da1139ca144bf
yugykim <yugyeongk92@gmail.com>
2022-10-11 23:21:37 -0600

    build orderEntity TypeORM table and GraphQL

---
+++ src/orders/entities/order.entity.ts
@@ -0,0 +1,63 @@
+import {
+  Field,
+  Float,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
+import { IsEnum } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Dish } from 'src/restaurants/entities/dish.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+import { OrderItem } from './order.item.entity';
+
+export enum OrderStatus {
+  Pending = 'Pending',
+  Cooking = 'Cooking',
+  PickedUp = 'PickedUp',
+  Delived = 'Delived',
+}
+
+registerEnumType(OrderStatus, { name: 'OrderStatus' });
+
+@InputType('OrderInputType', { isAbstract: true }) //graphql
+@ObjectType()
+@Entity()
+export class Order extends CoreEntity {
+  @Field(() => User, { nullable: true })
+  @ManyToOne(() => User, (user) => user.orders, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  customer?: User;
+
+  @Field(() => User, { nullable: true })
+  @ManyToOne(() => User, (user) => user.rides, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  driver?: User;
+
+  @Field(() => Restaurant, { nullable: true })
+  @ManyToOne(() => Restaurant, (rest) => rest.orders, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  restaurant: Restaurant;
+
+  @Field(() => [OrderItem])
+  @ManyToMany(() => OrderItem)
+  @JoinTable() //Owner side relationship in ManyToMany
+  items: Dish[];
+
+  @Column({ nullable: true })
+  @Field(() => Float, { nullable: true })
+  total: number;
+
+  @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.Pending })
+  @Field(() => OrderStatus)
+  @IsEnum(OrderStatus)
+  status: OrderStatus;
+}
