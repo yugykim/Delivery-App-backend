42c9dc388b7924897bd75e483a2b59e47a6b98be
yugykim <yugyeongk92@gmail.com>
2022-10-11 23:19:19 -0600

    changed userRole enum 'client'->'customer'

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -10,11 +10,12 @@ import * as bcrypt from 'bcrypt';
 import { InternalServerErrorException } from '@nestjs/common';
 import { IsBoolean, IsEmail, IsEnum, IsString } from 'class-validator';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { Order } from 'src/orders/entities/order.entity';
 
 //=enumerate
 export enum UserRole {
   Owner = 'Owner',
-  Client = 'Client',
+  Customer = 'Customer',
   Delivery = 'Delivery',
 }
 
@@ -47,6 +48,14 @@ export class User extends CoreEntity {
   @Field(() => [Restaurant])
   @OneToMany(() => Restaurant, (restaurant) => restaurant.owner)
   restaurants: Restaurant[];
+
+  @Field(() => [Order])
+  @OneToMany(() => Order, (order) => order.customer)
+  orders: Order[];
+
+  @Field(() => [Order])
+  @OneToMany(() => Order, (order) => order.driver)
+  rides: Order[];
 
   @BeforeInsert()
   @BeforeUpdate() // hash password when users update their password. beforeUpdate is triggered when specific entity is update
