ab72648169384d74e9eab550629968b6cb469376
yugykim <yugyeongk92@gmail.com>
2022-10-12 23:42:06 -0600

    Changed Items's type to OrderItem[] fromDish[]

--- src/orders/entities/order.item.entity.ts
+++ src/orders/entities/order.item.entity.ts
@@ -1,6 +1,6 @@
 import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Dish, DishChoice } from 'src/restaurants/entities/dish.entity';
+import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Column, Entity, ManyToOne } from 'typeorm';
 
 //ordrItem option is only one, which is user made
