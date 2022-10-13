2261ff7a336512e07b4e6fe860b0875f54118100
yugykim <yugyeongk92@gmail.com>
2022-10-12 23:43:51 -0600

    add OrderItem, Dish in TypeOrmModule.forFeature

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order.item.entity';
 import { OrderResolver } from './order.resolver';
 import { OrderService } from './order.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Restaurant])],
+  imports: [TypeOrmModule.forFeature([Order, Restaurant, OrderItem, Dish])],
   providers: [OrderResolver, OrderService],
   exports: [OrderService],
 })
