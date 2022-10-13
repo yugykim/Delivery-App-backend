c28991effd494e96abc4c0c48ace7dd58fcab5aa
yugykim <yugyeongk92@gmail.com>
2022-10-12 23:44:30 -0600

    OrderItem and DIsh are injectRepo,

--- src/orders/order.service.ts
+++ src/orders/order.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
-import { User } from 'src/users/entities/user.entity';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { GetOrdersOutput, GetOrdersInput } from './dtos/get-order.dto';
 import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order.item.entity';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(Order)
     private readonly orders: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private readonly orderItems: Repository<OrderItem>,
+    @InjectRepository(Dish)
+    private readonly dishes: Repository<Dish>,
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
   ) {}
@@ -19,26 +26,118 @@ export class OrderService {
     customer: User,
     { restaurantID, items }: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
-    const restaurant = await this.restaurants.findOne({
-      where: {
-        id: restaurantID,
-      },
-    });
-    if (!restaurant) {
-      return {
-        ok: false,
-        error: 'could not find a restaurant',
-      };
-      /* 
+    try {
+      const restaurant = await this.restaurants.findOne({
+        where: {
+          id: restaurantID,
+        },
+      });
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: 'could not find a restaurant',
+        };
+      }
+      let orderFinalPrice = 0;
+      const orderItems: OrderItem[] = []; //making empty array
+      for (const item of items) {
+        const dish = await this.dishes.findOne(item.dishId);
+        if (!dish) {
+          // abort this whole thing
+          return {
+            ok: false,
+            error: 'Dish is not found.',
+          };
+        }
+        let dishFinalPrice = dish.price;
+        for (const itemOption of item.options) {
+          const dishOption = dish.options.find(
+            (dishOption) => dishOption.name === itemOption.name,
+          );
+          if (dishOption) {
+            if (dishOption.extra) {
+              dishFinalPrice += dishOption.extra;
+            } else {
+              const dishOptionChoice = dishOption.choice.find(
+                (optionChoice) => optionChoice.name === itemOption.choice,
+              );
+              if (dishOptionChoice) {
+                if (dishOptionChoice.extra) {
+                  dishFinalPrice += dishOptionChoice.extra;
+                }
+              }
+            }
+          }
+        }
+        orderFinalPrice += dishFinalPrice;
+        const orderItem = await this.orderItems.save(
+          this.orderItems.create({
+            dish,
+            options: item.options,
+          }),
+        );
+        orderItems.push(orderItem);
+      }
       const order = await this.orders.save(
         this.orders.create({
           customer,
+          restaurant,
+          total: orderFinalPrice,
+          items: orderItems,
         }),
       );
-      console.log(order); */
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error: 'Could not make an order.',
+      };
     }
-    items.forEach((item) => {
-      console.log(item.options);
-    });
+  }
+
+  async getOrders(
+    user: User,
+    { status }: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    try {
+      let orders: Order[];
+      if (user.role === UserRole.Customer) {
+        orders = await this.orders.find({
+          where: {
+            customer: user,
+            status,
+          },
+        });
+      } else if (user.role === UserRole.Delivery) {
+        orders = await this.orders.find({
+          where: {
+            driver: user,
+            status,
+          },
+        });
+      } else if (user.role === UserRole.Owner) {
+        const restaurants = await this.restaurants.find({
+          where: {
+            owner: user,
+            status,
+          },
+          relations: ['orders'],
+        });
+        console.log(restaurants);
+        orders = restaurants.map((restaurant) => restaurant.orders).flat();
+        console.log(orders);
+        return {
+          ok: true,
+          orders,
+        };
+      }
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not get orders',
+      };
+    }
   }
 }
