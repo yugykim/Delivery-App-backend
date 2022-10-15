7f6f00eb52b9db7de2ee3ca8836a96dea2aa2725
yugykim <yugyeongk92@gmail.com>
2022-10-14 22:25:32 -0600

    Ws-socket starts Once when serV Start,rturn token

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,16 +1,18 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { resolveSoa } from 'dns';
-import { Observable } from 'rxjs';
+import { JwtService } from 'src/jwt/jwt.service';
+import { UsersService } from 'src/users/users.service';
 import { AllowedRoles } from './role.decoratior';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
     const roles = this.reflector.get<AllowedRoles>(
       'roles',
       context.getHandler(),
@@ -19,15 +21,24 @@ export class AuthGuard implements CanActivate {
       return true;
     }
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    const user = gqlContext['user'];
-
-    if (!user) {
+    const token = gqlContext.token;
+    if (token) {
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        const { user } = await this.userService.findById(decoded['id']);
+        if (!user) {
+          return false;
+        }
+        gqlContext['user'] = user;
+        if (roles.includes('Any')) {
+          return true;
+        }
+        return roles.includes(user.role);
+      } else {
+        return false;
+      }
+    } else {
       return false;
     }
-    if (roles.includes('Any')) {
-      return true;
-    }
-    return roles.includes(user.role);
   }
-  //this tell us we continue or not
 }
