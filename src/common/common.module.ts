76c3a64b28e725ffae06596f05a38853a4936cdf
yugykim <yugyeongk92@gmail.com>
2022-10-16 10:00:46 -0600

    Add Subscriptions PUBSUB,Accessing Everywhere

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
+import { PUB_SUB } from './common.constants';
 
-@Module({})
+const pubsub = new PubSub(); //should be one in the whole app
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: PUB_SUB,
+      useValue: pubsub,
+    },
+  ],
+  exports: [PUB_SUB],
+})
 export class CommonModule {}
