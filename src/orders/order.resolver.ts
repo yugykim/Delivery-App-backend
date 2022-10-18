e8bd7a31c5806f6a23b950704922a1602e1e72e2
yugykim <yugyeongk92@gmail.com>
2022-10-17 22:59:28 -0600

    Subscription CookedOrder/OrderUpdate/TakeOrder

--- src/orders/order.resolver.ts
+++ src/orders/order.resolver.ts
@@ -3,13 +3,20 @@ import { Args, Mutation, Resolver, Query, Subscription } from '@nestjs/graphql';
 import { PubSub } from 'graphql-subscriptions';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { Role } from 'src/auth/role.decoratior';
-import { NEW_PENDING_ORDER, PUB_SUB } from 'src/common/common.constants';
+import {
+  NEW_PENDING_ORDER,
+  PUB_SUB,
+  NEW_COOKED_ORDER,
+  NEW_ORDER_UPDATE,
+} from 'src/common/common.constants';
 import { EditDishOutput } from 'src/restaurants/dtos/edit-dish.dto';
 import { User } from 'src/users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { EditOrderInput, EditOrderOutput } from './dtos/edit-order.dto';
 import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 import { GetOrdersOutput, GetOrdersInput } from './dtos/get-orders.dto';
+import { OrderUpdateInput } from './dtos/order-update.dto';
+import { TakeOrderInput, TakeOrderOutput } from './dtos/take-order.dto';
 import { Order } from './entities/order.entity';
 import { OrderService } from './order.service';
 
@@ -55,6 +62,7 @@ export class OrderResolver {
     return this.orderService.editOrder(user, editOrderInput);
   }
 
+  //user order food, and pending order (customer)
   @Subscription(() => Order, {
     filter: ({ pendingOrders: { ownerId } }, _, { user }) => {
       //Inside payload, there in an ownerId, Inside context, there is an User
@@ -66,5 +74,43 @@ export class OrderResolver {
   @Role(['Owner'])
   pendingOrders() {
     return this.pubsub.asyncIterator(NEW_PENDING_ORDER);
+  }
+
+  //cooked order when user is owner.
+  @Subscription(() => Order)
+  @Role(['Owner'])
+  cookedOrder() {
+    return this.pubsub.asyncIterator(NEW_COOKED_ORDER);
+  }
+
+  //cooked order when user is owner.
+  @Subscription(() => Order, {
+    filter: (
+      { orderUpdates: order }: { orderUpdates: Order },
+      { input }: { input: OrderUpdateInput },
+      { user }: { user: User },
+    ) => {
+      if (
+        order.driverId !== user.id &&
+        order.customerId !== user.id &&
+        order.restaurant.ownerId !== user.id
+      ) {
+        return false;
+      }
+      return order.id === input.id;
+    },
+  })
+  @Role(['Any'])
+  orderUpdates(@Args('input') orderUpdateInput: OrderUpdateInput) {
+    return this.pubsub.asyncIterator(NEW_ORDER_UPDATE);
+  }
+
+  @Mutation(() => TakeOrderOutput)
+  @Role(['Delivery'])
+  takeOrder(
+    @AuthUser() driver: User,
+    @Args('input') takeOrderInput: TakeOrderInput,
+  ): Promise<TakeOrderOutput> {
+    return this.orderService.takeOrder(driver, takeOrderInput);
   }
 }
