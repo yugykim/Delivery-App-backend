5519c22fbb7d25c7a9ae94b2d7fe2f5184bbec18
yugykim <yugyeongk92@gmail.com>
2022-10-30 18:40:35 -0600

    Build Payment Module, Import Payment&ResT

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { Payment } from './entities/payment.entities';
+import { PaymentResolver } from './payments.resolver';
+import { PaymentsService } from './payments.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
+  providers: [PaymentsService, PaymentResolver],
+})
 export class PaymentsModule {}
